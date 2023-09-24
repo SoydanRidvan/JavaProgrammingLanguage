d5d820ef366b84a4fe81e9955ca7fe198be1353a
Rıdvan Soydan <ridvansoydan0711@gmail.com>
2023-09-24 11:20:07 +0300

    First Commit

---
+++ src/Primitives.java
@@ -0,0 +1,33 @@
+
+
+public class Primitives {
+
+    public static void main(String[] args) {
+
+
+
+            // DataType var;
+                   // => 3 types variables => Local, Instance, Static
+            int a; // => This is local variable, usage is limited in the scope of method, outside of this block you cannot use it
+                   // => Local variables does not have default value, you need to set variable before useit
+            a = 10;
+            System.out.println(a);
+
+            double b = a; // implicit casting
+
+            System.out.println(b);
+
+            long c = 200;
+
+            short d = (short)c;
+
+
+
+
+        }
+
+
+    }
+
+
+
